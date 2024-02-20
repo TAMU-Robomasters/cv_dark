0750173aa58f79098febdcbf412bb26816dbc47e
Jeff Hykin <jeff.hykin@gmail.com>
2024-02-20 09:12:50 -0600

    fix

--- commands/patch_venv.js
+++ commands/patch_venv.js
@@ -1,6 +1,9 @@
-import { FileSystem, glob } from "https://deno.land/x/quickr@0.6.62/main/file_system.js"
-import { run, hasCommand, throwIfFails, zipInto, mergeInto, returnAsString, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo, } from "https://deno.land/x/quickr@0.6.62/main/run.js"
-import { Console, clearAnsiStylesFrom, black, white, red, green, blue, yellow, cyan, magenta, lightBlack, lightWhite, lightRed, lightGreen, lightBlue, lightYellow, lightMagenta, lightCyan, blackBackground, whiteBackground, redBackground, greenBackground, blueBackground, yellowBackground, magentaBackground, cyanBackground, lightBlackBackground, lightRedBackground, lightGreenBackground, lightYellowBackground, lightBlueBackground, lightMagentaBackground, lightCyanBackground, lightWhiteBackground, bold, reset, dim, italic, underline, inverse, strikethrough, gray, grey, lightGray, lightGrey, grayBackground, greyBackground, lightGrayBackground, lightGreyBackground, } from "https://deno.land/x/quickr@0.6.62/main/console.js"
+#!/usr/bin/env -S deno run --allow-all
+
+// also see: https://discourse.nixos.org/t/auto-patch-downloaded-binaries/7124
+import { FileSystem, glob } from "https://deno.land/x/quickr@0.6.63/main/file_system.js"
+import { Console } from "https://deno.land/x/quickr@0.6.63/main/console.js"
+import { run, Stdout, Stderr } from "https://deno.land/x/quickr@0.6.63/main/run.js"
 
 // depends on
     // system_tools having a variable named "cc" with a lib folder
@@ -8,7 +11,8 @@ import { Console, clearAnsiStylesFrom, black, white, red, green, blue, yellow, c
     // VIRTUAL_ENV existing
 
 if (Console.env.VIRTUAL_ENV) {
-    const ccLibsFolder = JSON.parse(Console.env.__FORNIX_NIX_PATH_EXPORT_FILE).libPathFor.cc
+    // const ccLibsFolder = JSON.parse(Deno.env.get("VIRKSHOP_NIX_SHELL_DATA")).libraryPaths['cc']
+    const ccLibsFolder = JSON.parse(FileSystem.sync.read(Deno.env.get("__FORNIX_NIX_PATH_EXPORT_FILE"))).libPathFor['cc']
     const venvInfo = await FileSystem.info(Console.env.VIRTUAL_ENV)
     if (!ccLibsFolder) {
         console.log(`\n\n        I expected the system_tools.yaml to have a \`saveVariableAs: cc\`\n        Usually something like:\n            - (package):\n                load: [ "stdenv", "cc", "cc" ]\n                saveVariableAs: cc\n\n        but I didn't see one, or the lib folder didnt exist\n`)
@@ -20,8 +24,7 @@ if (Console.env.VIRTUAL_ENV) {
                 run`patchelf --set-rpath ${ccLibsFolder} ${eachSharedObjectFile}`
             }
         } else {
-            console.log(`I don't see a venv folder, so it's kinda hard for me to patch it`)
-            // 
+            console.log("\n\n        NOTE: you're probably going to need to do a 'pip install pysurvive'\n        and THEN exit, and run virkshop/enter again\n\n")
         }
     }
 }
