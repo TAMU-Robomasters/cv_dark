db5a94dbcb918717348a40cdd7b799057f9050e8
David <davids.atx1@gmail.com>
2024-04-12 17:40:19 -0500

    red extract

--- scripts/redextract.py
+++ scripts/redextract.py
@@ -1,3 +1,21 @@
 # script to extract red from an image
 
 import cv2
+import 
+
+# IMAGE_PATH = r""
+vid = cv2.VideoCapture(0)
+
+def redextract(image_path):
+    image = cv2.imread(image_path)
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # lower mask (0-10)
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+    mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+    # upper mask (170-180)
+    lower_red = np.array([170,50,50])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
