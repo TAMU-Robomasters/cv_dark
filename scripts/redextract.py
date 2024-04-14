fc497d8cad7f5a688c756244ca1faa9f0cc45194
David <davids.atx1@gmail.com>
2024-04-14 15:49:15 -0500

    script to extract red from image, cannot do more without actual LEDs

--- scripts/redextract.py
+++ scripts/redextract.py
@@ -1,21 +1,47 @@
 # script to extract red from an image
 
 import cv2
-import 
+import numpy as np
 
 # IMAGE_PATH = r""
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-def redextract(image_path):
-    image = cv2.imread(image_path)
+
+def redextract(fr):
+    # image = cv2.imread(frame)
+    image = fr
     img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # lower mask (0-10)
-    lower_red = np.array([0,50,50])
-    upper_red = np.array([10,255,255])
+    lower_red = np.array([0, int(.5 * 255), int(.35 * 255)])
+    upper_red = np.array([10, 255, 255])
     mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
 
     # upper mask (170-180)
-    lower_red = np.array([170,50,50])
-    upper_red = np.array([180,255,255])
+    lower_red = np.array([170, int(.5 * 255), int(.35 * 255)])
+    upper_red = np.array([180, 255, 255])
     mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+    mask = mask0 + mask1
+
+    out_img = image.copy()
+    out_img[np.where(mask == 0)] = 0
+
+    out_hsv = img_hsv.copy()
+    out_hsv[np.where(mask == 0)] = 0
+    return out_img, out_hsv
+
+
+while vid.isOpened():
+    ret, frame = vid.read()
+    if not ret:
+        print("ERROR: Could not read frame")
+        break
+    img, hsv = redextract(frame)
+    cv2.imshow('redextract', img)
+    cv2.imshow('original', frame)
+    if cv2.waitKey(1) == 27:
+        break
+
+vid.release()
+cv2.destroyAllWindows()
