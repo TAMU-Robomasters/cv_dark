21f892e2645301a031ee23f274d6c96978f26e14
David <davids.atx1@gmail.com>
2024-04-14 16:20:21 -0500

    variable for lower s and lower v values

--- scripts/redextract.py
+++ scripts/redextract.py
@@ -7,24 +7,27 @@ import numpy as np
 vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 def hsv_to_opencvhsv(h, s, v):
-    return np.array(h // 2, int((s / 100) * 255), int((v / 100) * 255))
+    return np.array([h // 2, int((s / 100) * 255), int((v / 100) * 255)])
 
 def redextract(fr):
     # image = cv2.imread(frame)
     image = fr
     img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+    LOWER_S = 60
+    LOWER_V = 35
+
     # lower mask (0-10)
     # lower_red = np.array([0, int(.5 * 255), int(.35 * 255)])
     # upper_red = np.array([10, 255, 255])
-    lower_red = hsv_to_opencvhsv(0, 50, 35)
+    lower_red = hsv_to_opencvhsv(0, LOWER_S, LOWER_V)
     upper_red = hsv_to_opencvhsv(20, 100, 100)
     mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
 
     # upper mask (170-180)
     # lower_red = np.array([170, int(.5 * 255), int(.35 * 255)])
     # upper_red = np.array([180, 255, 255])
-    lower_red = hsv_to_opencvhsv(340, 50, 35)
+    lower_red = hsv_to_opencvhsv(340, LOWER_S, LOWER_V)
     upper_red = hsv_to_opencvhsv(360, 100, 100)
     mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
 
