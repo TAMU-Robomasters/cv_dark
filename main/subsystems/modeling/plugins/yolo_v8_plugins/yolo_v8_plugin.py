42738d866490faae217b02e6efc1b7bf56709b27
xavier <tamurobomasters@gmail.com>
2024-05-24 13:05:27 -0500

    fix misleading log  message

--- main/subsystems/modeling/plugins/yolo_v8_plugins/yolo_v8_plugin.py
+++ main/subsystems/modeling/plugins/yolo_v8_plugins/yolo_v8_plugin.py
@@ -19,7 +19,7 @@ class Yolov8(object):
         # config check
         assert acceleration in ['tensor_rt', 'gpu', 'cpu', None ]
         self.acceleration = acceleration
-        if acceleration == 'gpu' and torch.cuda.is_available():
+        if acceleration in [ 'tensor_rt', 'gpu' ] and torch.cuda.is_available():
             print("[modeling]   gpu_acceleration: ENABLED\n")
             # loaded_model = loaded_model.to(torch.device("cuda"))
             self.device = torch.device("cuda")
