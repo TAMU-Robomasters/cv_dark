654eb8c0243a23e82d9604dd4b286227ddbf0111
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2024-09-19 16:52:36 -0500

    Copypaste code from a tutorial

--- drew_detection/main.py
+++ drew_detection/main.py
@@ -4,7 +4,16 @@
 # Started by Drew Wingfield
 # on 2024/09/19
 
+# usbipd attach --wsl --busid=4-1
+
+# With lots of help (and code) from the docs:
+# https://docs.opencv.org/
+
 #region Imports
+import numpy
+import cv2 as cv
+
+
 #endregion Imports
 
 
@@ -19,10 +28,32 @@
 #region Functions
 #endregion Functions
 
-
 #region Procedural
 
-print("Hello, world!")
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+ 
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
+
+print("program complete!")
 
 #endregion Procedural
 
