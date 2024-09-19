ed9f8e40a33aaf0134175b0e8bed5a14a5d53cbb
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2024-09-19 15:58:27 -0500

    Add a folder and main.py for my detection

---
+++ drew_detection/main.py
@@ -0,0 +1,29 @@
+
+
+# drew_detection/main.py
+# Started by Drew Wingfield
+# on 2024/09/19
+
+#region Imports
+#endregion Imports
+
+
+#region Constants
+#endregion Constants
+
+
+#region Classes
+#endregion Classes
+
+
+#region Functions
+#endregion Functions
+
+
+#region Procedural
+
+print("Hello, world!")
+
+#endregion Procedural
+
+# -- end of file --
