139bea8fc70f3a149f09016988ebdb2245002a51
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2024-09-22 16:19:20 -0500

    Create a filter_yellow function to filter out all yellows

--- drew_detection/main.py
+++ drew_detection/main.py
@@ -10,7 +10,7 @@
 # https://docs.opencv.org/
 
 #region Imports
-import numpy
+import numpy as np
 import cv2 as cv
 import sys
 
@@ -27,6 +27,27 @@ import sys
 
 
 #region Functions
+def filter_yellow(frame, save_output=False):
+    # Threshold of blue in HSV space 
+    yellow_lower = np.array([50, 120, 85])
+    yellow_upper = np.array([80, 255, 255])
+
+    # preparing the mask to overlay 
+    mask = cv.inRange(img, yellow_lower, yellow_upper) 
+
+    # The black region in the mask has the value of 0, 
+    # so when multiplied with original image removes all non-blue regions 
+    result = cv.bitwise_and(img, img, mask = mask) 
+
+    if save_output:
+        cv.imwrite("/home/drewwingfield/TAMURobomasters/cv_dark.git/drew_detection/source/yellow.png", 
+            result)
+        cv.imwrite("/home/drewwingfield/TAMURobomasters/cv_dark.git/drew_detection/source/mask.png", 
+            mask)
+    
+    return result
+
+
 #endregion Functions
 
 #region Procedural
@@ -36,10 +57,12 @@ import sys
 img = cv.imread(cv.samples.findFile("/home/drewwingfield/TAMURobomasters/cv_dark.git/drew_detection/source/WIN_20240921_11_21_53_Pro.jpg"))
 if img is None:
     sys.exit("Could not read the image.")
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("/home/drewwingfield/TAMURobomasters/cv_dark.git/drew_detection/source/WIN_20240921_11_21_53_Pro.png", img)
+
+filter_yellow(img,save_output=True)
+
+
+
+
 
 # When everything done, release the capture
 print("program complete!")
