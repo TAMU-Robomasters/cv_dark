1538efcdbfd898ac67a75f527a967277f940b02a
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2024-10-11 17:03:17 -0500

    Add a python script for pose estimation and example video

---
+++ main/subsystems/nugget_receptacle/receptacle_pose.py
@@ -0,0 +1,15 @@
+
+# receptacle_pose.py
+# Started by Drew Wingfield
+# on 2024-10-11
+
+# Gets the pose of the receptacle given a frame
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("receptacle_pose was called as main.")
+
