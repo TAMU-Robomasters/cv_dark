fa4432df2e70adae9df38749c719246b60e4cc2f
kahzmic <159095560+kahzmic@users.noreply.github.com>
2024-11-06 11:14:23 -0600

    add my laptop camera as a videostream class for remote testing

--- main/subsystems/video_stream.py
+++ main/subsystems/video_stream.py
@@ -8,6 +8,8 @@ if config.hardware.camera == 'zed':
     from subsystems.video_streaming.zed import VideoStream
 elif config.hardware.camera == 'realsense':
     from subsystems.video_streaming.realsense import VideoStream
+elif config.hardware.camera == 'juan':
+    from subsystems.video_streaming.juan import VideoStream
 else:
     from subsystems.video_streaming.simulation import VideoStream
 
