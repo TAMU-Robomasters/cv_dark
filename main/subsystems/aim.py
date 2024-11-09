5eb73b83f90e4bbe50b69ef450bf8c5970cbffa3
kahzmic <159095560+kahzmic@users.noreply.github.com>
2024-11-08 18:32:43 -0600

    -

--- main/subsystems/aim.py
+++ main/subsystems/aim.py
@@ -114,6 +114,7 @@ def when_bounding_boxes_refresh():
                 frame_delay = time_since_last_measurement
             if frame_delay > 0.255:
                 print(f"Warming frame delay of {frame_delay} is really high")
+            print(frame_delay)
             # this contains the prediction of all the state variables [x, y, z, vx, vy, vz, ax, ay, az]
             forward_prediction = kf_3d.forward_predict(frame_delay) 
             target_3d_prediction = Position((forward_prediction[0], forward_prediction[3], forward_prediction[6]))
