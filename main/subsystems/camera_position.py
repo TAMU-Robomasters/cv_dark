d31287163e63e40a21805ed4e528e0d36e0ad67d
Jai <jaia_patel@student.midwayisd.org>
2025-02-07 18:39:53 -0600

    made aruco estimation work with cv version 3.7.0.72

--- main/subsystems/camera_position.py
+++ main/subsystems/camera_position.py
@@ -44,7 +44,7 @@ MARKER_SIZE = 150 # mm
 
 
 # define an empty custom dictionary with
-aruco_dict = cv2.aruco.Dictionary(5, 7)
+aruco_dict = cv2.aruco.Dictionary(5, 5)
 
 # add empty bytesList array to fill with
 aruco_dict.bytesList = np.empty(shape=(5, 4, 4), dtype=np.uint8)
