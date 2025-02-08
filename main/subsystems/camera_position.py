3ecba33f789e1300f74f5f71bfa5c2e310c998b3
Jai <jaia_patel@student.midwayisd.org>
2025-02-07 19:09:02 -0600

    made aruco estimation work with cv version 3.7.0.72

--- main/subsystems/camera_position.py
+++ main/subsystems/camera_position.py
@@ -93,6 +93,8 @@ aruco_dict.bytesList[4] = cv2.aruco.Dictionary_getByteListFromBits(e_pattern)
 
 parameters = cv2.aruco.DetectorParameters()
 detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+
+print(cv2.aruco.Dictionary.readDictionary(aruco_dict))
 # Kalman filter init
 
 #TODO reformat this stuff
