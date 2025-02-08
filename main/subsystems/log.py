a26619a357aeb135d8503c4f671bffb84c1b5d6c
Kestrel41x4 <kestrel41x4@gmail.com>
2025-02-07 18:33:59 -0600

    test1

--- main/subsystems/log.py
+++ main/subsystems/log.py
@@ -99,6 +99,7 @@ def plot_calculation():
 def when_finished_processing_frame():
     global color_video_writer
     global depth_video_writer
+    print("REACHED FRAME PROCESSING")
     
     # import data
     frame_number            = runtime.frame_number
@@ -144,13 +145,13 @@ def when_finished_processing_frame():
 
 
         
-    if display_live_frames:
-            if depth_compatible:
-                cv2.imshow("depth", depth.img)
-                plt.show(block=False)
-                plt.close()
-            cv2.imshow("main", image.img)
-            cv2.waitKey(1) # doesn't actually wait  
+    # if display_live_frames:
+    #         if depth_compatible:
+    #             cv2.imshow("depth", depth.img)
+    #             plt.show(block=False)
+    #             plt.close()
+    #         cv2.imshow("main", image.img)
+    #         cv2.waitKey(1) # doesn't actually wait  
     
     if should_save_frame:
         color_video_writer.add_frame(runtime.color_image)
