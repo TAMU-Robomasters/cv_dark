160b8c755659ef1f910ef5a782f6fbd577a9e6fb
Jai <jaia_patel@student.midwayisd.org>
2025-02-07 18:25:34 -0600

    made aruco estimation work with cv version 3.7.0.72

--- main/subsystems/camera_position.py
+++ main/subsystems/camera_position.py
@@ -4,7 +4,7 @@ import numpy as np
 from time import time_ns
 
 from super_map import LazyDict
-from subsystems.vision_position_estimation import a_pattern, b_pattern, c_pattern, d_pattern, e_pattern, \
+from subsystems.vision_position_estimation import b_pattern, c_pattern, d_pattern, e_pattern, \
     rotation_matrix_to_euler_angles, id_to_letter
 #TODO rework this so that it's pulling from the main kalman filter script
 #NOTE should probably change how this structured 
@@ -48,7 +48,14 @@ aruco_dict = cv2.aruco.Dictionary(6, 5, 0)
 
 # add empty bytesList array to fill with
 aruco_dict.bytesList = np.empty(shape=(5, 4, 4), dtype=np.uint8)
-#
+
+a_pattern = np.array([
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1], ])
+
 aruco_dict.bytesList[0] = cv2.aruco.Dictionary_getByteListFromBits(a_pattern)
 # add_marker(aruco_dict, 0, a_pattern)
 # add_marker(aruco_dict, 1, b_pattern)
