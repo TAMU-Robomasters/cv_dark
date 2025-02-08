12ba5f2659ce3e8cdfd6aab1bdaff1e798d23752
Kestrel41x4 <kestrel41x4@gmail.com>
2025-02-07 19:12:00 -0600

    test1

--- main/subsystems/log.py
+++ main/subsystems/log.py
@@ -97,7 +97,7 @@ def plot_calculation():
     print("target 3d point 1", target_3d_point[1])
     print("target 3d point 2", target_3d_point[2])
     print("point runtime", runtime.aiming.target_3d)
-    print("prediction runtime", runtime.aiming.target)
+    print("prediction runtime", runtime.aiming.target_3d_prediction)
     print("FLOATERR",floaterr)
     print("Time",elapsed_time)
     return  elapsed_time,floaterr
