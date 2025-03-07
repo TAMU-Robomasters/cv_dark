4128af30b55162ab41fdf10a4e9b4777b5280837
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-06 19:23:52 -0600

    fix boot message

--- commands/xavier/boot_control/setup_boot_script.js
+++ commands/xavier/boot_control/setup_boot_script.js
@@ -88,14 +88,6 @@ const contentsOfInfiniteRunScript = `#!/usr/bin/env bash
 #       2. logs all output to $HOME/boot.log
 #       3. runs the following IN A LOOP: ${pathToOnBootCommand.replace(/\n/g,"")}
 
-echo "#"
-echo "#"
-echo "#"
-echo "# BOOTING UP XAVIER"
-echo "#"
-echo "#"
-echo "#"
-
 this_username=${shellEscape(userName)}
 this_home=${shellEscape(home)}
 boot_log=${shellEscape(pathToBootLogFile)}
@@ -112,6 +104,14 @@ rm -f "$boot_log"
 
 export HOME="$this_home"
 sudo -u "$this_username" -E this_username="$this_username" this_home="$this_home" -- ${shellEscape(pathToZsh)} -c ${shellEscape(`
+    echo "#"
+    echo "#"
+    echo "#"
+    echo "# BOOTING UP XAVIER"
+    echo "#"
+    echo "#"
+    echo "#"
+    
     while true
     do
         . ${shellEscape(envVarsFile)}
