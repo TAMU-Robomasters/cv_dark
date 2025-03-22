53368e9e5f380d586bb199cc7e0adca109860625
ketrel <Kestrel>
2025-03-21 19:17:12 -0500

    update

--- main/subsystems/aim.py
+++ main/subsystems/aim.py
@@ -121,7 +121,7 @@ def when_bounding_boxes_refresh():
             # this contains the prediction of all the state variables [x, vx, ax, y, vy, ay, z, vz, az]
             forward_prediction = kf_3d.forward_predict(frame_delay) 
             target_3d_prediction = Position((forward_prediction[0], forward_prediction[3], forward_prediction[6]))
-            runtime.aiming.storedPrediction.append(best_target_3d)
+            runtime.aiming.storedPrediction.append(target_3d_prediction)
     else:
         # if camera is not depth capable, find best box
         # mostly used for testing purposes
